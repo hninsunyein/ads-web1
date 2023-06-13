e56efbae17a0e472a192bfbaeb06744a9a8e0198
Hnin Su Nyein <41577939+hninsunyein@users.noreply.github.com>
2023-06-13 16:02:46 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// require("./dist/index.js")
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(5002);
